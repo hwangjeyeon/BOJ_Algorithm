8b32b3f8572cb89b33f4c345ccd7c13ab432aa3c
황제연 <kingpele9018@gmail.com>
2024-02-11 15:54:44 +0900

    Create 2752.java

---
+++ JAVA/2752.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * 풀이 과정:
+ * - 배열을 오름차순 정렬하면 된다.
+ *
+ * 시간복잡도: O(1)
+ * 공간복잡도: O(1)
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < 3; i++) {
+            bw.write(arr[i] + " ");
+        }
+
+        br.close();
+        bw.close();
+    }
+
+
+}
+
