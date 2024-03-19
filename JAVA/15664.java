654c3d76c8fc1969227289941a9bcb2531da839d
황제연 <kingpele9018@gmail.com>
2024-03-19 11:15:44 +0900

    Create 15664.java

---
+++ JAVA/15664.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * 풀이 과정:
+ * - 고민과 풀이:
+ * 재귀(백트래킹)
+ * 1. 함수식 backtracking(n,m, depth, i)
+ * 2. base condition if(m==depth)
+ * 3. 재귀식 (n,m,depth+1, i+1)
+ *
+ * - 이전 문제에서 다시 start 파라미터를 추가한다
+ *
+ * - 문제 해결:
+ *
+ *
+ * 시간복잡도: O(nlogm)
+ * 공간복잡도: O(nlogm)
+ *
+ */
+
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int[] input;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        st = new StringTokenizer(br.readLine());
+        input = new int[n];
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+        backtracking(n,m,0, 0);
+        bw.write(sb.toString());
+
+        br.close();
+        bw.close();
+    }
+
+    private static void backtracking(int n, int m, int depth, int start) {
+        if(m == depth){
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int before = 0;
+        for (int i = start; i < n; i++) {
+            if(!visited[i] && before != input[i]){
+                visited[i] = true;
+                arr[depth] = input[i];
+                before = input[i];
+                backtracking(n, m,depth+1, i+1);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
+
