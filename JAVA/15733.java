a034cc6c27a5e86d055340fe50ec428c76140535
황제연 <kingpele9018@gmail.com>
2024-10-11 22:17:57 +0900

    Create 15733.java

---
+++ JAVA/15733.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * 풀이 과정:
+ * - 고민과 풀이:
+ * 1. 그냥 출력문제.
+ *
+ * - 문제 해결:
+ *
+ *
+ * 시간복잡도: O(1)
+ * 공간복잡도: O(1)
+ *
+ */
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("I'm Sexy");
+
+        br.close();
+        bw.close();
+    }
+}
+
