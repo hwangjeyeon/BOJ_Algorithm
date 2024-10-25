eaa224900f02499dd3b6a1651536a629fb54a846
황제연 <kingpele9018@gmail.com>
2024-10-25 23:32:12 +0900

    Create 2393.java

---
+++ JAVA/2393.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * 풀이 과정:
+ * - 고민과 풀이:
+ * 1. 그냥 출력하는 문제.
+ *
+ * - 문제 해결:
+ *
+ *
+ * 시간복잡도: O()
+ * 공간복잡도: O()
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+
+        br.close();
+        bw.close();
+    }
+}
+
