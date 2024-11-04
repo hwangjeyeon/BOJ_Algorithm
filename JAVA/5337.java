3a1e0186a7d3bc6a84d3ff0e76cf1ba1e2ac65fe
황제연 <kingpele9018@gmail.com>
2024-11-04 18:30:10 +0900

    Create 5337.java

---
+++ JAVA/5337.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * 풀이 과정:
+ * - 고민과 풀이:
+ * 1. 출력 기초 문제
+ *
+ * - 문제 해결:
+ *
+ *
+ * 시간복잡도: O()
+ * 공간복잡도: O()
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+
+        br.close();
+        bw.close();
+    }
+
+}
+
