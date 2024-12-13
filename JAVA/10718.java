bf93188bcc235b4fe5279ac0219f14a91d103d67
황제연 <kingpele9018@gmail.com>
2024-12-13 23:14:51 +0900

    백준 10718번 - We love kriii
    백준 10718번 - We love kriii (누적합)

---
+++ JAVA/10718.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 과정:
+ *
+ * 해결방법:
+ *
+ * 시간복잡도: O()
+ * 공간복잡도: O()
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+
+        br.close();
+        bw.close();
+    }
+
+}
