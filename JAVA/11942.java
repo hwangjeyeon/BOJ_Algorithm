42fdd2cae01f6f65c79ec63cbc5ca70343d96da3
황제연 <kingpele9018@gmail.com>
2024-12-14 22:45:35 +0900

    백준 11942번 - 고려대는 사랑입니다.
    백준 11942번 - 고려대는 사랑입니다.

---
+++ JAVA/11942.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 과정:
+ *
+ * 해결방법:
+ *
+ * 시간복잡도: O()
+ * 공간복잡도: O()
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("고려대학교");
+        
+        br.close();
+        bw.close();
+    }
+
+}
