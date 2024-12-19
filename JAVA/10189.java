f95e5a19829df79ce5a3de2ee535890cc87cbf33
황제연 <kingpele9018@gmail.com>
2024-12-19 23:43:58 +0900

    백준 10189번 - Hook
    백준 10189번 - Hook

---
+++ JAVA/10189.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 과정:
+ *
+ * 해결방법:
+ *
+ * 시간복잡도: O()
+ * 공간복잡도: O()
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+
+        br.close();
+        bw.close();
+    }
+
+}
