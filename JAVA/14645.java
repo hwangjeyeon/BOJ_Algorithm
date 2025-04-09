712ff3d91a63c862d87f39fbaf43bb37d7e3c1b3
황제연 <kingpele9018@gmail.com>
2025-04-09 23:41:00 +0900

    백준 14645번 - 와이버스 부릉부릉
    백준 14645번 - 와이버스 부릉부릉 (기초)

---
+++ JAVA/14645.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 과정:
+ * 1. 그냥 출력하세요.
+ *
+ * 해결방법:
+ *
+ * 시간복잡도: O(1)
+ * 공간복잡도: O(1)
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+        }
+
+        bw.write("비와이");
+        br.close();
+        bw.close();
+    }
+
+}
