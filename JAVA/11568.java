d3dbe0c60007e0b36a726335901ebd9d0d5de3d9
황제연 <kingpele9018@gmail.com>
2025-04-10 20:35:27 +0900

    백준 11568번 - 민균이의 계략
    백준 11568번 - 민균이의 계략 (DP)

---
+++ JAVA/11568.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 과정:
+ * 1. 이중 포문 LIS로 풀면 되는 쉬운 문제다.
+ *
+ * 해결방법:
+ *
+ * 시간복잡도: O(n^2)
+ * 공간복잡도: O(n)
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        bw.write(max + "");
+        
+        br.close();
+        bw.close();
+    }
+
+}
