787cce38eedb2b435dd0f04cb1783a2f25d93f60
황제연 <kingpele9018@gmail.com>
2025-08-14 21:12:03 +0900

    백준 5717번 - 상근이의 친구들
    백준 5717번 - 상근이의 친구들 (구현)

---
+++ JAVA/5717.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 풀이 과정:
+ * 1. 합산해서 출력하면 정답이 된다
+ *
+ * 해결방법:
+ *
+ * 시간복잡도: O(1)
+ * 공간복잡도: O(1)
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0 ){
+                break;
+            }
+
+            bw.write((a+b)+"\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+
+}
